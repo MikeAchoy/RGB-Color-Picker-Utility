90f38d3ac35c653212bdfbbec58d6510b93708b4
MikeAchoy <97768019+MikeAchoy@users.noreply.github.com>
2023-06-01 15:13:20 -0500

    Update ErrorWindow.java

--- ErrorWindow.java
+++ ErrorWindow.java
@@ -5,35 +5,43 @@ import java.awt.event.ActionListener;
 
 public class ErrorWindow extends JFrame {
 
+    public ErrorWindow(char invalidValue){
+        // Set up window settings.
+        final int WINDOW_WIDTH = 300;
+        final int WINDOW_HEIGHT = 100;
+        final String WINDOW_NAME = "Error";
 
-    // TODO: Creat e nice layout for the error window.
-    // Set the text and make the button smaller and centered.
-
-    public ErrorWindow(){
-        // Set window init settings.
-        final int ERROR_WINDOW_WIDTH = 250;
-        final int ERROR_WINDOW_HEIGHT = 200;
-        final String ERROR_WINDOW_TITLE = "Value Error";
-
-        this.setSize(new Dimension(ERROR_WINDOW_WIDTH, ERROR_WINDOW_HEIGHT));
-        this.setTitle(ERROR_WINDOW_TITLE);
+        this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setTitle(WINDOW_NAME);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        // Create window components.
-        JLabel errorMessage = new JLabel();
-        String errorMessageText = "<h1>Error:</h1> <p1>Something test.</p1>";
+        // Create window objects.
+        JPanel errorMessagePanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1,3));
 
-        errorMessage.setText(errorMessageText);
-
+        JLabel errorMessage = new JLabel("Invalid Value: " + invalidValue + " bounds are [0, 250]");
         JButton okButton = new JButton("Ok");
+
         okButton.addActionListener(e -> {
             this.dispose();
         });
 
-        // Add window components to the window.
-        this.getContentPane().add(errorMessage);
-        this.getContentPane().add(okButton);
+        // Add empty labels and button to messagePanel to center button.
+        errorMessagePanel.add(errorMessage);
+        for(int i = 0; i < 3; i++){
+            if(i == 1){
+                buttonPanel.add(okButton);
+            }
+            else{
+                buttonPanel.add(new JLabel());
+            }
+        }
+
+        // Add window objects to the window.
+        this.getContentPane().add(errorMessagePanel, BorderLayout.NORTH);
+        this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 
     public void setVisible(){
